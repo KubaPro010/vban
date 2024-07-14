ae506219b3370d14ccb762f222d51442fca87ae9
KubaPro010 <kuba.pro010@gmail.com>
2024-07-14 14:20:10 +0200

    g

--- src/common/packet.c
+++ src/common/packet.c
@@ -125,7 +125,7 @@ static int packet_pcm_check(char const* buffer, size_t size)
 
 void packet_decode_service(char const* buffer) {
     struct VBanServiceData const* const data = (struct VBanServiceData*)PACKET_PAYLOAD_PTR(buffer);
-    logger_log(LOG_FATAL, (const char*)data->ApplicationName_ascii);
+    logger_log(LOG_ERROR, (const char*)data->ApplicationName_ascii);
     // TODO: Complete
 }
 
